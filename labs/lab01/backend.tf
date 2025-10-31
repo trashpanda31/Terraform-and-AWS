89e347249c64d14e47ccd85e05e702cfb50c53f8
Eliass Rozenbergs <149315973+trashpanda31@users.noreply.github.com>
2025-10-31 13:30:27 +0200

    Revert "PR(feat): add lab01"

--- labs/lab01/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "devops-state-504508177008"
-    key            = "labs/lab1-vpc-ec2/terraform.tfstate"
-    region         = "eu-north-1"
-    dynamodb_table = "DevOps-lock"
-    encrypt        = true
-  }
-}
